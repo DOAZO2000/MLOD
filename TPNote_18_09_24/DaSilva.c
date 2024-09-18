068b2b42aacd1a93a8607fea1fe35abbc40f1ab4
Leny DA SILVA <lenydasilva28@gmail.com>
2024-09-18 17:01:09 +0200

    Last version

--- TPNote_18_09_24/DaSilva.c
+++ TPNote_18_09_24/DaSilva.c
@@ -14,14 +14,20 @@ unsigned int sizeRepeat (int t1[], int t2[], unsigned int sizet1, unsigned int s
     }
     
 }
+
+
+
 int* repeat (int t1[], int t2[], unsigned int sizet1, unsigned int sizet2){
     if(sizet1==sizet2){
+
         unsigned int sizeTr = sizeRepeat(t1,t2,sizet1,sizet2);
         int* tr = malloc(sizeTr);
         int k=0;
-        for(int i =0; i<sizet1; i++){
+
+        for(int i =0; i<3; i++){
             for(int j=0; j<t1[i];j++){
-                *(tr+k+j)=t2[i];
+                tr[k]=t2[i];
+                k++;
             }
         }
 
@@ -32,13 +38,15 @@ int* repeat (int t1[], int t2[], unsigned int sizet1, unsigned int sizet2){
 void main(){
     int t1 [] = {1,2,4};
     int t2 [] = {10,3,8};
+
     unsigned sizet1 = sizeof(t1);
     unsigned sizet2 = sizeof(t2);
-    unsigned int sizeTr = sizeRepeat(t1,t2,sizet1,sizet2);
-    int* tr = malloc(sizeTr);
-    tr = repeat(t1,t1, sizet1, sizet2);
-    for(int i=0; i<(sizeTr/sizeof(1)); i++){
+
+    int *tr = repeat(t1,t1, sizet1, sizet2);
+    for(int i=0; i<7; i++){
         printf("%n ", tr[i]); 
     }
 
+
+    free(tr);
 }
