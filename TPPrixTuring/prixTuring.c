b13db5663cd3eec08137645265c8b13b897b2654
Leny DA SILVA <lenydasilva28@gmail.com>
2024-09-20 10:03:53 +0200

    20/09 - commit de la pause

--- TPPrixTuring/prixTuring.c
+++ TPPrixTuring/prixTuring.c
@@ -17,31 +17,57 @@
 #include <assert.h>
 
 
+typedef struct {
+	unsigned short year;
+	char *name;
+	char *achievement;
+} Winner;
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+int nombreDeLigne (FILE* f){
+	int nombreLigne =0;  
+	char buff[1024];	
+
+	while (fgets(buff,1023,f) != NULL) // traitement 
+	{
+		nombreLigne++;
+	}
+	rewind(f);
+	return nombreLigne;
+}
+
 int main(){
+
+	// creation des structs
 	char filename[] = "turingWinners.csv";
-	//char outputFilename[] = "out.csv";
-	FILE* f;
-	char test[10];
-	int test2;
+	char outputFilename[] = "out.csv";
+	FILE* fIn;
+	// FILE* fOut;
+	fIn = fopen(filename, "r");
+	// fOut = fopen(outputFilename, "r");
+
+	//creation des tests
+
+	//char test[2000];
+	//char test2[2000];
+	int nombreLigne;
+
+	nombreLigne = nombreDeLigne(fIn);
+	//fgets(test,2000,fIn);
+	//fgets(test2,2000,fIn);
 
 
-	f = fopen(filename, "r");
-	fgets(test,10,f);
-	test2 =fgetc(f);
+	//evaluation des tests
 
-	char* bloc[10];
-	int taillef;
+	printf("------7-----\n");
+	//printf("%s\n", test);
+	//printf("%s\n", test2);
+	printf("%d\n", nombreLigne);
 
-
-
-
-	printf("-----------\n");
-	printf("%s\n", test);
-	printf("%c\n", test2);
 	printf("-----------\n");
 
     // TODO
