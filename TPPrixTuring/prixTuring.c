1d968cd876babbee0728b3a960def9ba6664b71f
Leny DA SILVA <lenydasilva28@gmail.com>
2024-09-23 10:24:40 +0200

    commit de la pause 23/09

--- TPPrixTuring/prixTuring.c
+++ TPPrixTuring/prixTuring.c
@@ -26,9 +26,10 @@ typedef struct {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-int tailleChaine(char* buff){
+// strlen
+int tailleChaine(char* buff, char marker){
 	int taille=0;
-		while((buff[taille])!='\0')			//arret au \0
+		while((buff[taille])!=marker)			//arret au marker
 		{		
 			taille++;
 		}
@@ -47,75 +48,88 @@ int numberOfWinners (FILE* f){
 	return nombreLigne;
 }
 
-Winner creationWinner (FILE* f){
-	Winner res;
+Winner *creationWinner (FILE* f){
+	Winner *res = malloc(sizeof(Winner));
 	char buff[2048];
+	
+	// read one line
 	fgets(buff,2047,f);
 
 	int k=0;					//compteur a travers mon buffer
-// ecriture de year -------------
 	while(buff[k]!=';')			//arret au premier point virgule.
 	{		
 		k++;
 	}
 	buff[k] = '\0';
 	char* next;
-	res.year = strtol(buff,&next,10);
+	res->year = strtol(buff,&next,10);
+
+	printf("year = %d\n",res->year);
+
 	k++;
 // ecriture de name
 
-	int tailleName=tailleChaine(&(buff[k]));
-	res.name = malloc((1+tailleName)*sizeof(char));
+	int tailleName=tailleChaine(&(buff[k]),';');
+	res->name = malloc((tailleName)*sizeof(char));
 	// strcpy
+	printf("taille Name =%d\n",tailleName);
 	for(int i=0;i<tailleName;i++)			
 	{		
-		res.name[i]=buff[k];
+		res->name[i]=buff[k];
 		k++;
 	}
-	k++;
-	res.name[k]='\0';
+	res->name[k]='\0';
+	printf("name = %s\n",res->name);
+
 	k++;
 
 // ecriture de achievement
 
-	int tailleAchievement=tailleChaine(&(buff[k]));
-	res.achievement = malloc((1+tailleAchievement)*sizeof(char));
+	int tailleAchievement=tailleChaine(&(buff[k]),'\0');
+	res->achievement = malloc((tailleAchievement)*sizeof(char));
 
 	for(int i=0;i<tailleAchievement;i++)			
 	{		
-		res.achievement[i]=buff[k];
+		res->achievement[i]=buff[k];
 		k++;
 	}
-	k++;
-	res.achievement[k]='\0';
+	res->achievement[k]='\0';
+	printf("achievement = %s\n",res->achievement);
+	
 	return res;
 }
 
 
 Winner** tabWinner(FILE* f)
 {
-	int nombreLigne = numberOfWinners(f);    						  //Nombre de ligne du fichier = nombre de Winner dans le tableau.
+	int nombreLigne = numberOfWinners(f); 
+	printf("Total winners = %d\n",nombreLigne);
 	Winner** tabWinner = malloc(nombreLigne*sizeof(Winner*));    //allocation dynamic de taille d'un tableau de Winner.
 	
 	for(int k=0;k<nombreLigne;k++)
 	{
-		tabWinner[k] = malloc(sizeof(Winner));
-		*(tabWinner[k])=creationWinner(f);					//ecriture du winner actuel dans le tableau.
-
+		tabWinner[k]=creationWinner(f);					//ecriture du winner actuel dans le tableau.
 	}
-	rewind(f);
 	return tabWinner;
-
 }
 
 void print1Winner(FILE*f,Winner* winner){
-	char buff[1024];
+	char buff[2048];
+
 	sprintf(buff,"%d",(*winner).year);
-	buff[4] = '\n';
+	buff[4] = ';';
+	buff[5] = '\0';
+	// char* buff1=strcpy(&(buff[5]),(*winner).name);
+	// buff1[tailleChaine((*winner).name,'\0')]=';';
+	// strcpy(&buff1[1+tailleChaine((*winner).name,'\0')],(*winner).achievement);
 	
-	fwrite(buff,sizeof(char),tailleChaine(buff),f);
-	fwrite((*winner).name,sizeof(char),tailleChaine((*winner).name),f);
-	fwrite((*winner).achievement,sizeof(char),tailleChaine((*winner).achievement),f);
+	// printf("buff1 =%s\n",buff1);
+	printf("buffer du print1winnner =%s\n",buff);
+
+	//fwrite(buff,sizeof(char),tailleChaine(buff,'\0'),f);
+	int nbSuccess=fwrite(winner->name,sizeof(char),strlen(winner->name),f);
+	fwrite(winner->achievement,sizeof(char),tailleChaine(winner->achievement,'\0'),f);
+	printf("nombre de caracteres ecris =%d\n",nbSuccess);
 
 }
 
@@ -133,6 +147,9 @@ void infosAnnee(int annee){
 
 }
 
+void sortTuringWinnersByYear(FILE* f){
+
+}
 
 
 int main(int argc, char** argv){
@@ -145,21 +162,23 @@ int main(int argc, char** argv){
 	fIn = fopen(filename, "r");
 	fOut = fopen(outputFilename, "w");
 
-	Winner** winners = tabWinner(fIn);
-
-	printWinners(fIn,fOut,winners);
+	Winner *one = creationWinner(fIn);
+	print1Winner(fOut,one);
+	printf("error ? =%d",ferror(fOut));
+	
+	//Winner** winners = tabWinner(fIn);
+	//rewind(f);
+	//printWinners(fIn,fOut,winners);
 
 	//evaluation des tests
 
-	printf("------testEcriture2-----\n");
+	// printf("------testEcriture2-----\n");
 
 	//printf("année =%d\n",testWinner.year);
 	//printf("nom(s)=%s\n",testWinner.name);
 	//printf("accomplissement(s)=%s\n",testWinner.achievement);d
 
-	printf("-----------\n");
-
-    // TODO
+	// printf("-----------\n");
 
 	return EXIT_SUCCESS;
 }
